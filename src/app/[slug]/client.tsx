42e78851fd2d76629cf5cebd37ccbcd638487238
AbortEnshtein <ilikesalolo@gmail.com>
2025-03-08 19:41:58 +0300

    08.03.25

--- src/app/[slug]/client.tsx
+++ src/app/[slug]/client.tsx
@@ -1,32 +1,25 @@
 'use client'
 
 import '@/app/[slug]/cards.css'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
  
-export default function ClientComponent({children, level}: {children: React.ReactNode, level: JSON}) {
-  const enter = (e) => {
-    const el = document.getElementById(e.target.id)
-    const css = window.getComputedStyle(el)
-    el.setAttribute('style', `height: ${parseInt(css.height)+20}px; width: ${parseInt(css.width)+10}px`)
-  }
-  const exet = (e) => {
-    const el = document.getElementById(e.target.id)
-    el.setAttribute('style', '')
-  }
+export default function ClientComponent({children, level, cards}: {children: React.ReactNode, level: {id:string,text:string,img:string[],card_count:number, card_text:string[],card_img:string[],card_path:string[]}, cards: React.JSX.Element[]}) {
   return (
-    <div id='main'>
-      <img id='img' src={level.img[0]} alt="" />
-      <img id='img2' src={level.img[1]} alt="" />
-      <img id='img3' src={level.img[2]} alt="" />
-      <Link id='s' className='s' href={'/'+`${level.id}`+'/map'} />
-      <img id='i' className='s' src="https://2.downloader.disk.yandex.ru/preview/560f4bac09dd8ba0f5928068f69af2894ac37eff3edc68fbc3ca00788183bf8c/inf/mkp4XlqTjjs3HkjJDFzmFc3_icVoAhjRTy85Vc9xKSdg5z93QC6UybefkAMyewXX4bKYwTXIzJAUwd52p30STw%3D%3D?uid=863728945&filename=free-sticker-location-10630133.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=863728945&tknv=v2&size=1898x912" alt="" />
-      <div id='text-box'>{level.text}</div>
-      <div id='card-box'>
-        <Link onMouseEnter={(e)=>{enter(e)}} onMouseLeave={(e)=>{exet(e)}} id='b1' className='card' href={'/'+`${level.id}`+'1'}>{level.card1}</Link>
-        <Link onMouseEnter={(e)=>{enter(e)}} onMouseLeave={(e)=>{exet(e)}} id='b2'className='card' href={'/'+`${level.id}`+'2'}>{level.card2}</Link>
-        <Link onMouseEnter={(e)=>{enter(e)}} onMouseLeave={(e)=>{exet(e)}} id='b3' className='card' href={'/'+`${level.id}`+'3'}>{level.card3}</Link>
-      </div>
+    <div id='g'>
+      <img className='im' id='img' src={level.img[0]} alt="" />
+      <img className='im' id='img2' src={level.img[1]} alt="" />
+      <img className='im' id='img3' src={level.img[2]} alt="" />
+      <div id='main'>
+        <div className='back-box'>
+          <img className='back' src="https://2.downloader.disk.yandex.ru/preview/560f4bac09dd8ba0f5928068f69af2894ac37eff3edc68fbc3ca00788183bf8c/inf/mkp4XlqTjjs3HkjJDFzmFc3_icVoAhjRTy85Vc9xKSdg5z93QC6UybefkAMyewXX4bKYwTXIzJAUwd52p30STw%3D%3D?uid=863728945&filename=free-sticker-location-10630133.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=863728945&tknv=v2&size=1898x912" alt="" />
+          <Link className='back' href={'/'+`${level.id}`+'/map'}>{' '}</Link>
+        </div>
+        <div id='text-box'><p id='t-b'/><p id='t2'>{level.text}</p></div>
+        <div id='card-box'>
+          {cards}
+        </div>
+     </div>
     </div>
   )
 }
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,18 +1,32 @@
 import { notFound } from 'next/navigation'
 import ClientComponent from './client'
+import Link from 'next/link';
+
 
 export default async function ServerComponent({params}: {params: Promise<{ slug: string }>}) {
     const slug = (await params).slug
     const { Pool } = require('pg')
     const pool = new Pool({
-        user: "owner",
-        host: "localhost",
-        database: "Clicker",
-        password: "admin",
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PAS,
         port: 5432,
     });
-    const result = await pool.query(`Select * From levels Where id=${slug}`)
-    const level = result.rows[0]
+    const result = await pool.query(`SELECT * FROM levels WHERE id='${slug}'`)
+    const level:{id:string,text:string,img:string[],card_count:number, card_text:string[],card_img:string[],card_path:string[]} = result.rows[0]
+    let bod:React.JSX.Element[] = []
     if (level===undefined) {notFound()}
-    return (<ClientComponent children={undefined} level={level} />)
+    for (let i=0; i<level.card_count;i++) {
+        const styling = {
+            backgroundImage: `url('${level.card_img[i]}')`
+        }
+        if (level.card_path==null){
+            bod.push(<Link className='card' style={styling} href={'/'+`${level.id}`+`${i+1}`}><p id='t'>{level.card_text[i]}</p><p id='text'/></Link>)
+        } else {
+            bod.push(<Link className='card' style={styling} href={'/'+ `${level.card_path[i]}`}><p id='t'>{level.card_text[i]}</p><p id='text'/></Link>)
+        }
+        
+    }
+    return (<ClientComponent cards={bod} children={undefined} level={level} />)
 }
