33b9728bf590bff1dbafd2c61912ac471e089e28
AbortEnshtein <ilikesalolo@gmail.com>
2025-03-20 23:26:57 +0300

    20.03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
         <Link id='l1' className='butlink' href={'/1'}>Глава 1</Link>
         <Link id='l2' className='butlink' href={'/2'}>Глава 2</Link>
         <Link id='l3' className='butlink' href={'/3'}>Глава 3</Link>
+        <Link id='l4' className='butlink' href={'/4'}>Тест</Link>
       </div>
     </div>
   )
